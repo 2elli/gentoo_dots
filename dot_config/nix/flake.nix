527780f5fc3b4cdc640bf2643f83bb22b10c589f
elliot <elliot@ivy.rip>
2026-03-03 08:29:34 -0600

    simplify system packages flake

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -1,47 +1,26 @@
 {
-    description = "System Packages";
+  description = "System Packages";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    outputs = {self, nixpkgs, ...} @ inputs: let
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in {
-        packages."x86_64-linux" = {
-            haskell = pkgs.buildEnv {
-                name = "haskell";
-                paths = with pkgs; [
-                    ghc
-                    haskellPackages.haskell-language-server
-                    haskellPackages.hlint
-                    haskellPackages.fourmolu
-                    haskellPackages.stack
-                ];
-            };
-
-            ai = pkgs.buildEnv {
-                name = "ai";
-                paths = with pkgs; [
-                    opencode
-                ];
-            };
-
-            security = pkgs.buildEnv {
-                name = "security";
-                paths = with pkgs; [
-                    metasploit
-                ];
-            };
-
-            default = pkgs.buildEnv {
-                name = "system-packages";
-                paths = with self.packages."x86_64-linux"; [
-                    haskell
-                    ai
-                    security
-                ];
-            };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system} = {
+        default = pkgs.buildEnv {
+          name = "system-packages";
+          paths = with pkgs; [
+            ghc
+            haskellPackages.stack
+            opencode
+            metasploit
+            bitwarden-desktop
+          ];
         };
+      };
     };
 }
